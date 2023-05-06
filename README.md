89993bd1692152811b9f23ad1891b8f1e3d2e878
dongryong <dongryong@gmail.com>
2023-05-05 17:50:05 -0700

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 python manage.py collectstatic
+
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/dongryong/src.git
+git push -u origin main
--- products/models.py
+++ products/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User 
 
 class Product(models.Model):
     name = models.CharField(max_length=220)
@@ -11,4 +12,11 @@ class Purchase(models.Model):
     product = models.ForeignKey(Product,on_delete=models.CASCADE)
     price = models.PositiveIntegerField()
     quanity = models.PositiveIntegerField()
+    total_price = models.PositiveIntegerField(blank=True)
+    salesman = models.ForeignKey(User, on_delete=models.CASCADE)
+    date = models.DateTimeField(auto_now_add=True)
+
     
+
+
+
